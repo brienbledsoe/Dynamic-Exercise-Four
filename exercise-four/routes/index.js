7dc31555f8eb5b0e448fdbeff6112d588f750713
Brien Bledsoe <bab619@nyu.edu>
2019-11-05 16:13:14 -0500

    changed file structure

--- exercise-four/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router = express.Router();
-
-router.get('/', (req,res) => (res.send('This is About The API')));
-//router.get('/me', (req, res) => (res.send('About me')));
-
-
-module.exports = router;

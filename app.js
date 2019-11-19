8cb0ee3abf4e3f00b713035e227fa9925a545317
Brien Bledsoe <bab619@nyu.edu>
2019-11-19 15:58:38 -0500

    created form

--- app.js
+++ app.js
@@ -2,19 +2,23 @@
 const express = require('express');
 const app= express();
 const port = process.env.PORT || 4000;
+const papth = require("path");
 
 const indexRoute = require('./routes/index.js');
 const aboutRoute = require('./routes/posts.js');
 //Submit data
 const submitRoute = require('./routes/submit.js');
 /*Serve files in express*/
-
+app.use(express.static(path.join(__dirname,"public")));
 app.use('/', indexRoute);
 app.use('/about', aboutRoute);
 //set submit data route
 app.use('/submit', submitRoute);
 //Here we're going to set up a form to submit from. We already have an
-//endpoint 
+//endpoint
+app.use("/submit-form", (req,res) =>
+  res.sendFile("/public/form.html", {root: __dirname})
+);
 /*
 app.get('/', (req,res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

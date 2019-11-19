65000c17a86836b40fd8710e16b13ebc161f8abb
Brien Bledsoe <bab619@nyu.edu>
2019-11-19 16:00:41 -0500

    testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app= express();
 const port = process.env.PORT || 4000;
-const papth = require("path");
+const path = require("path");
 
 const indexRoute = require('./routes/index.js');
 const aboutRoute = require('./routes/posts.js');
